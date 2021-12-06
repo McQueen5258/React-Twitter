d6278a6e254c8a6d752dc55c41dde409132e38ba
Eric <uwizeyeeric@icloud.com>
2021-12-07 01:09:00 +0800

    test

---
+++ build/precache-manifest.bf07dad6c157bf45c131e1e330c4ed71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "007495261f25ba58a7ff",
+    "url": "/test/static/css/main.ac33ccbe.chunk.css"
+  },
+  {
+    "revision": "007495261f25ba58a7ff",
+    "url": "/test/static/js/main.00749526.chunk.js"
+  },
+  {
+    "revision": "81fbc468f0317fec149b",
+    "url": "/test/static/js/1.81fbc468.chunk.js"
+  },
+  {
+    "revision": "5ad1e219c88e053e0b1e",
+    "url": "/test/static/js/runtime~main.5ad1e219.js"
+  },
+  {
+    "revision": "08355cb60d1c8f7bd8884ee2ba701da4",
+    "url": "/test/index.html"
+  }
+];
